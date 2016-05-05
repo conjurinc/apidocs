1e328af8756ad0ecad398254cc62d86557f2cb13
Kerry Munz <kerry@conjur.net>
2016-05-05 11:48:30 -0400

    switch to conjur_version 4.7

--- hooks.js
+++ hooks.js
@@ -188,14 +188,12 @@ hooks.beforeValidation('Role > Create > Create a new role', function(transaction
 
 // Role exists returns 200 with no body (as is expected with HEAD) but apiary fails parsing a body that doesn't exist
 // because it sees the Content-type is set to json - change it to text/plain
-['Role > Exists > Determine whether a role exists'].forEach(function(route) {
-    hooks.beforeValidation(route, function(transaction) {
-        if (transaction.real.statusCode === transaction.expected.statusCode) {
-            console.log('Skipping header checks');
-            transaction.real.headers = transaction.expected.headers;
-            transaction.real.body = transaction.expected.body;
-        }
-    });
+hooks.beforeValidation('Role > Exists > Determine whether a role exists', function(transaction) {
+      if (transaction.real.statusCode === 200) {
+          console.log('Skipping header checks');
+          transaction.real.headers = transaction.expected.headers;
+          transaction.real.body = transaction.expected.body;
+      }
 });
 
 /*
--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-CONJUR_VERSION=${CONJUR_VERSION:-"5.0"}
+CONJUR_VERSION=${CONJUR_VERSION:-"4.7"}
 DOCKER_IMAGE=${DOCKER_IMAGE:-"registry.tld/conjur-appliance-cuke-master:$CONJUR_VERSION-stable"}
 NOKILL=${NOKILL:-"0"}
 PUBLISH=${PUBLISH:-"0"}
